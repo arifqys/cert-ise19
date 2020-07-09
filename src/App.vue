16dbfa04874d32f13c64be807c1b1912692dee62
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2020-07-09 10:00:42 +0700

    install tailwindcss & add styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <div id="app">
-    <h1>ISE! 2019 Certificate</h1>
-    <div class="tab">
-      <button class="tab-links" @click="mode = 'BIONIX'">
+  <div class="container mx-auto px-4">
+    <h1 class="text-center text-3xl font-extrabold py-5">ISE! 2019 Certificate</h1>
+    <div class="flex justify-center">
+      <button class="px-6 py-3 mx-1 font-bold rounded-full" :class="mode === 'BIONIX' ? 'active' : null"  @click="mode = 'BIONIX'">
         BIONIX
       </button>
-      <button class="tab-links" @click="mode = 'ICON'">
+      <button class="px-6 py-3 mx-1 font-bold rounded-full" :class="mode === 'ICON' ? 'active' : null" @click="mode = 'ICON'">
         ICON
       </button>
     </div>
-    <div class="content">
-      <h2>{{mode}}</h2>
-      <label for="email">Masukkan email</label>
-      <input type="text" v-model.lazy="email" id="email" placeholder="johndoe@gmail.com">
-      <table class="list" v-if="certList.length > 0">
+    <div class="container mx-auto max-w-md mt-10 bg-gray-200 rounded-lg">
+      <h2 class="text-center text-5xl font-extrabold tracking-widest relative" style="top: -40px">{{mode}}</h2>
+      <div class="p-10 pt-0">
+      <div class="pb-5">
+        <label for="email" class="block text-xs p-1">Masukkan email</label>
+        <input type="text" class="block px-5 py-3 rounded-lg text-sm text-gray-800 max-w-full" v-model.lazy="email" id="email" placeholder="johndoe@gmail.com">
+      </div>
+      <table class="text-sm w-full" v-if="certList.length > 0">
         <tr v-for="data in certList" :key="data.no_surat">
-          <td>{{data.email}}</td>
-          <td>{{data.nama}}</td>
-          <td><button @click="downloadCert(data.nama, data.no_surat.toString(), mode)">Download</button></td>
+          <td class="py-3 pr-3">{{data.nama}}</td>
+          <td class="py-3 pl-3"><button class="text-xs px-4 py-2 bg-white rounded" @click="downloadCert(data.nama, data.no_surat.toString(), mode)">Download</button></td>
         </tr>
       </table>
-      <p v-if="certList.length === 0 && email">Sertifikat {{mode}} dengan email {{email}} tidak dapat ditemukan.</p>
+      <p v-if="certList.length === 0 && email">Sertifikat {{mode}} dengan email <span class="text-xs p-1 bg-yellow-100 font-mono font-semibold rounded">{{email}}</span> tidak dapat ditemukan.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -67,18 +70,36 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600;700;800&display=swap');
+
+html, body {
+  font-family: 'Montserrat';
+  font-size: 20px;
   color: #2c3e50;
-  margin-top: 60px;
 }
-.list {
-  margin: auto;
+
+input, button {
+  font-family: 'Montserrat';
 }
-.list td {
-  padding: 10px 20px;
+
+input:focus, button:active {
+  outline: none;
+}
+
+.active {
+  background-color: #63b3ed;
+  color: #ffffff;
+}
+
+@media (max-width: 600px) {
+  html, body {
+    font-size: 18px;
+  }
+}
+
+@media (max-width: 400px) {
+  html, body {
+    font-size: 16px;
+  }
 }
 </style>
